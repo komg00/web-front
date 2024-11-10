6d342ee2af85a761c77587b28020d7baf0589725
komg00 <komg00@naver.com>
2024-11-10 22:28:15 +0900

    [feat] Add Header

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        lightGreen: "#F7FBFA",
+        dark2: "#666666",
+      },
+    },
+  },
+  plugins: [],
+};
