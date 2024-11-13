50198337c81b7b5fd3db535599af46a2ee3b739e
komg00 <komg00@naver.com>
2024-11-13 10:28:02 +0900

    [feat] Add Club picker

---
+++ src/components/layout/ClubPicker.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { clubs } from "data/clubs";
+
+export default function ClubPicker() {
+  return (
+    <div className="w-full h-16 xl:h-19 bg-white flex gap-4 xl:gap-6 mb-auto mx-auto p-2 justify-center items-center border-b-2 border-gray-200 overflow-x-auto ">
+      <p className="text-dark1 text-xs lg:text-base xl:text-lg ml-1 whitespace-nowrap">
+        ALL
+      </p>
+      {clubs.map((club, index) => (
+        <div className="flex items-center ">
+          <img
+            key={index}
+            src={club.imagePath}
+            alt={club.title}
+            className="w-8 h-8"
+          />
+          <p className="hidden md:block text-dark1 text-xs lg:text-base xl:text-lg ml-0 lg:ml-1 whitespace-nowrap">
+            {club.title}
+          </p>
+        </div>
+      ))}
+    </div>
+  );
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import Sidebar from "components/layout/Sidebar";
 import { homeMenu } from "data/menu";
+import ClubPicker from "components/layout/ClubPicker";
 
 export default function Home() {
   return (
-    <div
-      className=" bg-light3 flex"
-      style={{ minHeight: "calc(100vh - 82px)" }}
-    >
+    <div className="bg-light3 flex" style={{ minHeight: "calc(100vh - 82px)" }}>
       <Sidebar menu={homeMenu} />
-      <div>test</div>
+      <div className="flex-1">
+        <ClubPicker />
+      </div>
     </div>
   );
 }
