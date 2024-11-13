121261b0b1cfd9ddfff9219f63d8fc6677e6870f
komg00 <komg00@naver.com>
2024-11-13 12:53:38 +0900

    [feat] Add Feed UI

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,6 +3,7 @@ import Sidebar from "components/layout/Sidebar";
 import { homeMenu } from "data/menu";
 import ClubPicker from "components/layout/ClubPicker";
 import WritePost from "components/home/WritePost";
+import Feed from "components/home/Feed";
 
 export default function Home() {
   return (
@@ -10,8 +11,9 @@ export default function Home() {
       <Sidebar menu={homeMenu} />
       <div className="flex-1">
         <ClubPicker />
-        <main className="w-full flex justify-center items-center">
+        <main className="w-full flex flex-col justify-center items-center mb-12">
           <WritePost />
+          <Feed />
         </main>
       </div>
     </div>
