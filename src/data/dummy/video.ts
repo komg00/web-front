31945599396411d29cfab96bec4ab30e194c8d6d
komg00 <komg00@naver.com>
2024-11-25 22:23:41 +0900

    [feat] Add dummy data for video

---
+++ src/data/dummy/video.ts
@@ -0,0 +1,75 @@
+interface Video {
+  index: number;
+  title: string;
+  image_path: string;
+}
+
+export const recommend_video: Video[] = [
+  {
+    index: 1,
+    title: "2024 KBO 리그 최고의 수비수들은 누굴까? | 2024 KBO 리그 수비상",
+    image_path:
+      "https://i.ytimg.com/vi/7WuxxwHvWYY/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDNRf_JPfVQF5KIx6fr04NQidVaGg",
+  },
+  {
+    index: 2,
+    title:
+      "[O.T.R] 두 번의 승리 그러나 | 2024 WBSC 프리미어12 야구 국가대표 크보직캠",
+    image_path:
+      "https://i.ytimg.com/vi/U_og8yPr1SQ/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAqKtKwTQJ_L2RFRv6bF5gNj1fYqA",
+  },
+  {
+    index: 3,
+    title:
+      "[무해설] 대만 타이베이돔 혼자 걷는 느낌은? |2024 WBSC 프리미어12 야구 국가대표 크보직캠",
+    image_path:
+      "https://i.ytimg.com/vi/UCNEu5d4odI/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLC5vMCWKrpohlPzqYBZryDQbLuLEw",
+  },
+  {
+    index: 4,
+    title:
+      "이제 다 '우리팀' 야수야!!! | 2024 WBSC 프리미어12 야구 국가대표 야수",
+    image_path:
+      "https://i.ytimg.com/vi/dvWgmy0bgKw/hqdefault.jpg?sqp=-oaymwE2CNACELwBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARgdIDoofzAP&rs=AOn4CLBil4fhVJeCNG78M9VJeXOFKWA7Bg",
+  },
+  {
+    index: 5,
+    title:
+      "[삼성 vs KIA] '반격 시작!' 한 점 추격하는 KIA 타이거즈 | 10.28 | KBO 모먼트 | KS 5차전 | 2024 KBO 포스트시즌",
+    image_path:
+      "https://i.ytimg.com/vi/Gu-8NaI-Bv8/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCxYSxl9r-iUBawmutt83pFy92r4g",
+  },
+];
+
+export const team_video: Video[] = [
+  {
+    index: 1,
+    title: "[미공개영상] 선수들에게 N년 전 영상을 보여줬더니 (+ 커피차)",
+    image_path:
+      "https://i.ytimg.com/vi/z3Fw5_alYZo/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLACvDYouPwlytrUJU5-C3dPNDbXLw",
+  },
+  {
+    index: 2,
+    title: "90년대생만 아는 노래? 노래만으로도 즐거운 챔필 훈련장 🎶",
+    image_path:
+      "https://i.ytimg.com/vi/MT9NIdXLzDY/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBEChMQ2H6JuoyZ-z8ieT3rz_1-Zw",
+  },
+  {
+    index: 3,
+    title: "‘700만 조회수' 구독자들의 최애♥ 콘텐츠는? [무인갸메라]",
+    image_path:
+      "https://i.ytimg.com/vi/YT_WpB8PTGY/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCx37x9v53W-XcaUKfULCT7DI1erQ",
+  },
+  {
+    index: 4,
+    title: "이곳이 바로 펑고 지옥 😇 (positive)",
+    image_path:
+      "https://i.ytimg.com/vi/226yd9Ks6eI/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB1e4w8h9mUAvCZcWjb5tcgxqvxCA",
+  },
+  {
+    index: 5,
+    title: "‘중계로는 볼 수 없음’ 2024 한국시리즈 외장하드 털이",
+    image_path:
+      "https://i.ytimg.com/vi/riTtE0QzuVc/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDPJPOS8G2TgQgmnqCJ5GXXhqYmKA",
+  },
+];
