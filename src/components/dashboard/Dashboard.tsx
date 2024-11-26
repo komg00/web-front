2275658c4932bea3077b265ccddd6c32771de79d
komg00 <komg00@naver.com>
2024-11-26 21:00:38 +0900

    [chore] Move Game Score layout into DashboardPage

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,40 +1,10 @@
 import React from "react";
-import { teamInfo, matchInfo, scoreboard } from "data/dummy/dashboard";
+import { matchInfo } from "data/dummy/dashboard";
 import Scoreboard from "./Scoreboard";
 
 export default function Dashboard() {
   return (
     <div className="w-full">
-      <div className="flex flex-col bg-white px-14 py-1 xl:py-4 text-dark1">
-        <h1 className="text-[45px] font-bold px-10 pt-4">{teamInfo.name}</h1>
-        <div className="w-4/5 flex text-22px px-16 pt-3 my-1 justify-between items-center">
-          <div className="flex flex-col items-center">
-            <p className="text-dark2">경기</p>
-            <p className="font-bold">{teamInfo.games}</p>
-          </div>
-          <div className="flex flex-col items-center">
-            <p className="text-dark2">승</p>
-            <p className="font-bold">{teamInfo.wins}</p>
-          </div>
-          <div className="flex flex-col items-center">
-            <p className="text-dark2">패</p>
-            <p className="font-bold">{teamInfo.defeats}</p>
-          </div>
-          <div className="flex flex-col items-center">
-            <p className="text-dark2">무</p>
-            <p className="font-bold">{teamInfo.ties}</p>
-          </div>
-          <div className="flex flex-col items-center">
-            <p className="text-dark2">승률</p>
-            <p className="font-bold">{teamInfo.winRate}</p>
-          </div>
-          <div className="flex flex-col items-center">
-            <p className="text-dark2">최근 10경기</p>
-            <p className="font-bold">{teamInfo.recentGames}</p>
-          </div>
-        </div>
-      </div>
-
       <div className="mx-16 mt-16 mb-2">
         <div className="bg-white shadow-sm my-8 px-24 py-10 flex justify-between items-center">
           <div className="flex items-center pl-10">
